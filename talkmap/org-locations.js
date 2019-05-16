0ae6c52f5478b1f360317bb29497a7f413229f92
vayyappan <49045019+vayyappan@users.noreply.github.com>
2019-05-16 16:11:26 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Baltimore, MD, USA",
+    39.2904,
+    -76.6122
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Paris, France",
+    48.8566,
+    2.3522
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Montreal, Canada",
+    45.5017,
+    -73.5673
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Atlanta, GA, USA",
+    33.7490,
+    -84.3880
   ]
 ];
